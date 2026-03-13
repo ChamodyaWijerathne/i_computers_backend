2c5445d59c856e44f4f7d485e9c2f1dfc34a1ee4
Chamodya Wijerathne <chamodyawijerathne096@gmail.com>
2026-03-13 21:33:53 +0530

    Day 28 I

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { createUser, loginUser } from "../controllers/userController.js"
+import { createUser, getUser, loginUser } from "../controllers/userController.js"
 
 const userRouter = express.Router()
 
 userRouter.post("/", createUser)//localhost:3000/users
 userRouter.post("/login", loginUser) //localhost:3000/users/login
+userRouter.get("/profile", getUser)
 
 export default userRouter
